529599fa1e654be5dc202c1cd3a865ea4461b079
Rob Sokolowski <rpsoko@gmail.com>
2021-09-14 00:24:24 -0400

    got some json encoding but there seem to be some differences in ES cloud vs normal

--- src/Pages/Search.elm
+++ src/Pages/Search.elm
@@ -1,6 +1,7 @@
 module Pages.Search exposing (Model, Msg, page)
 
 import Components.Styling as S
+import Debug
 import Effect exposing (Effect)
 import ElasticSearch as ES exposing (..)
 import Element exposing (..)
@@ -10,9 +11,12 @@ import Element.Events exposing (..)
 import Element.Font as Font
 import Element.Input as Input
 import Gen.Params.Search exposing (Params)
+import Http
+import Json.Decode as JD
 import Page
 import Request
 import Shared
+import Url exposing (Protocol(..))
 import View exposing (View)
 
 
@@ -50,6 +54,7 @@ init _ =
 type Msg
     = SearchTextChanged String
     | UserClickedSearch
+    | SearchResponded (Result Http.Error SearchResponse)
 
 
 update : Msg -> Model -> ( Model, Effect Msg )
@@ -68,7 +73,7 @@ update msg model =
                     { query =
                         ES.Term
                             { name = "president_name"
-                            , value = model.searchText
+                            , value = StringValue model.searchText
                             , boost = Nothing
                             , queryName = Nothing
                             }
@@ -76,8 +81,16 @@ update msg model =
                     }
             in
             ( model
-            , Effect.none
+            , Effect.fromCmd <| submitSearchRequest searchRequest
             )
+
+        SearchResponded result ->
+            case result of
+                Ok str ->
+                    ( model, Effect.none )
+
+                Err errs ->
+                    ( model, Effect.none )
 
 
 
@@ -132,3 +145,50 @@ viewElements model =
             , label = el [ centerX ] <| Element.text "Go:"
             }
         ]
+
+
+
+-- HTTP
+-- TODO: Does this belong in ElasticSearch.elm? How do I map Msgs to Pages then??
+
+
+host =
+    "https://fir-sandbox.ent.eastus2.azure.elastic-cloud.com"
+
+
+endpoint =
+    "/api/as/v1/engines/presidential-approval-ratings-dev/documents"
+
+
+submitSearchRequest : ES.SearchRequest -> Cmd Msg
+submitSearchRequest searchRequest =
+    let
+        endcodedRequest =
+            ES.encodeSearchRequest searchRequest
+
+        url =
+            host ++ endpoint
+    in
+    Http.request
+        { method = "POST"
+        , headers =
+            [ Http.header "Content-Type" "application/json"
+            , Http.header "Authorization" "Bearer TODO: Make this private!"
+            ]
+        , url = url
+        , body = Http.jsonBody endcodedRequest
+        , expect = Http.expectJson SearchResponded searchResponseDecoder
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+type alias SearchResponse =
+    { someField : String
+    }
+
+
+searchResponseDecoder : JD.Decoder SearchResponse
+searchResponseDecoder =
+    JD.map SearchResponse
+        (JD.at [ "someField" ] JD.string)
